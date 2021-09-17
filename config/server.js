ff2895cb8fe73e7d3a90c60848d9449e09a929f4
pardeeep <pardeep.iwebcode@gmail.com>
2021-09-17 17:50:22 +0530

    strapi backend

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e296805984995a061869df83c6892cca'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a4e55f9a-a5be-47f7-b964-e2245b6022f2'
+};
